029494ef9f0bdc8c9b340fd42ece6b2068ccc4bf
zygisn <kat@asdf.local>
2017-02-20 10:43:58 +0200

    First project commit

---
+++ Assets/scripts/floatingRock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floatingRock : moveObject {
+
+	[SerializeField] private Vector3 topPosition;
+	[SerializeField] private Vector3 bottomPosition;
+	[SerializeField] private int floatingSpeed = 3;
+
+	// Use this for initialization
+	void Start () {
+		topPosition = new Vector3(transform.localPosition.x, 16.0f);
+		bottomPosition = new Vector3(transform.localPosition.x, 9.0f);
+
+		StartCoroutine (Move(bottomPosition));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		base.moveObjectLeft();
+
+	}
+
+
+	IEnumerator Move(Vector3 target){
+
+		while(Mathf.Abs((target - transform.localPosition).y) > 0.2f){
+
+			Vector3 direction = target.y == topPosition.y ? Vector3.up : Vector3.down;
+			transform.localPosition += direction * (floatingSpeed * Time.deltaTime);
+
+			yield return null;
+		}
+
+		yield return new WaitForSeconds(0.5f);
+		Vector3 newTarget = target.y == topPosition.y ? bottomPosition : topPosition;
+
+		StartCoroutine (Move(newTarget));
+	}
+
+
+
+}
---
+++ Assets/scripts/moveObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveObject : MonoBehaviour {
+
+	public float objectSpeed = 4f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		moveObjectLeft();
+
+	}
+
+	protected virtual void moveObjectLeft(){
+
+		transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+
+		if(transform.localPosition.x <= -50){
+
+			Vector3 newPosition = new Vector3(32.9f, transform.position.y);
+			transform.position = newPosition;
+		}
+	}
+}
