64113c5c32b8316b0dc0b892f682617c92f20ca4
zygisn <kat@asdf.local>
2017-02-21 20:54:25 +0200

    Player idle/jump animation added

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class player : MonoBehaviour {
 
+	Animator anim;
+
 	// Use this for initialization
 	void Start () {
+
+		anim = GetComponent<Animator>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		jump();
+	}
+
+	public void jump(){
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+
+			anim.Play("jump");
+
+		}
+
+
 	}
 }
