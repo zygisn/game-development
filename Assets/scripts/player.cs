107adf8ef06ec04edc6185ae558a20e598866236
zygisn <kat@asdf.local>
2017-02-23 11:57:27 +0200

    Touch input for jumping added

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -6,11 +6,11 @@ public class player : MonoBehaviour {
 
 	Animator anim;
 
+
 	// Use this for initialization
 	void Start () {
 
 		anim = GetComponent<Animator>();
-		
 	}
 	
 	// Update is called once per frame
@@ -21,9 +21,10 @@ public class player : MonoBehaviour {
 
 	public void jump(){
 
-		if(Input.GetKeyDown(KeyCode.Space)){
+		if(Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0) && Input.GetTouch(0).phase == TouchPhase.Began){
 
 			anim.Play("jump");
+
 
 		}
 
